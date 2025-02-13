54e9d29ed09d478fa1b63531675fa91cb1aca64b
maldimz <pratamadimas03@gmail.com>
2025-02-13 15:00:07 +0700

    test

---
+++ src/app/contract.ts
@@ -0,0 +1,23 @@
+export const contractAddress = "0xFC2ab1978AD813869Df230Fa0d44f915311c6307";
+export const contractABI = [
+  {
+    inputs: [],
+    name: "getCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "increment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,15 @@ import Image from "next/image";
 import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "./client";
+import { InAppAACard } from "@/components/InAppAACard";
 
 export default function Home() {
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
-        <Header />
-
-        <div className="flex justify-center mb-20">
-          <ConnectButton
-            client={client}
-            appMetadata={{
-              name: "Example App",
-              url: "https://example.com",
-            }}
-          />
-        </div>
-
-        <ThirdwebResources />
-      </div>
-    </main>
+    <div className="min-h-screen bg-background">
+      <main className="container mx-auto mt-8 p-4">
+        <InAppAACard />
+      </main>
+    </div>
   );
 }
 
